445b2d6ff7ee56a5ac37f2c4cec9f8e1f9b5b8f3
DAEMMA <martinez.em246@gmail.com>
2026-05-14 00:59:43 -0500

    Optimización 2

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
@@ -95,6 +95,7 @@ export default async function RootLayout({
       lang={locale}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <link rel="manifest" href="/manifest.json" />
       <body className="min-h-screen flex flex-col text-white">
         <NextIntlClientProvider messages={messages} locale={locale}>
           <Toaster position="bottom-right" richColors theme="dark" closeButton />
